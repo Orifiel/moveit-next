c0b5bcfed81f25268192b9ef86e309243dea0439
CleitonMattos <cleitonmattos87@gmail.com>
2021-02-23 22:24:41 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { ExperienceBar } from '../components/ExperienceBar'
 export default function Home() {
   return (
     <div className="container">
+      <h1>TESTE</h1>
       <ExperienceBar/>
     </div>
   )
