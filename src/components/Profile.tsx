f88914ecb1873d1e93aaa5eec9b43711274eec79
Cleiton <49954230+Orifiel@users.noreply.github.com>
2023-12-08 18:58:39 -0300

    Linus Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,7 +9,7 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Fbiography%2FLinus-Torvalds&psig=AOvVaw3uP5HQ-WAG3yC0fgMHvA5P&ust=1702158396558000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCMz77ogIMDFQAAAAAdAAAAABAD" alt="Linus Torvalds"/>
+            <img src="Orifiel/moveit-next/public/readme_images/Linus Torvalds.png" alt="Linus Torvalds"/>
             <div>
                 <strong>Linus Torvalds</strong>
                 <p>
