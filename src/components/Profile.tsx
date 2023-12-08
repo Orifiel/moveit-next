a8bfc0d6f0e211211b39d4ad6142571624f4a2f4
Cleiton <49954230+Orifiel@users.noreply.github.com>
2023-12-08 18:48:04 -0300

    Change persona

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,9 +9,9 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="http://github.com/Orifiel.png" alt="Cleiton Mattos"/>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Fbiography%2FLinus-Torvalds&psig=AOvVaw3uP5HQ-WAG3yC0fgMHvA5P&ust=1702158396558000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCMz77ogIMDFQAAAAAdAAAAABAD" alt="Linus Torvalds"/>
             <div>
-                <strong>Cleiton Mattos</strong>
+                <strong>Linus Torvalds</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
                     Level {level}
