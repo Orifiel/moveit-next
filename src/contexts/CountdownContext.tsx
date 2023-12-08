feb3e0790be19999b9fa8fb65e92655f72b9e17a
Cleiton <49954230+Orifiel@users.noreply.github.com>
2023-12-08 17:49:28 -0300

    Update CountdownContext.tsx
    Reduzindo tempo para 1 min

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export function CountdownProvider({children}: CountdownProviderProps) {
 
     const { startNewChallenge } = useContext(ChallengesContext)
 
-    const [time, setTime] = useState(0.5 * 60);
+    const [time, setTime] = useState(1 * 60);
     const [isActive, seIstActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false)
 
