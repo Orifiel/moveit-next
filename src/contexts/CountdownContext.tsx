9ef9c97cb76505559d571b946a30d5d534838d24
Cleiton <49954230+Orifiel@users.noreply.github.com>
2024-02-05 15:34:18 -0300

    rollback confg

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export function CountdownProvider({children}: CountdownProviderProps) {
 
     const { startNewChallenge } = useContext(ChallengesContext)
 
-    const [time, setTime] = useState(1 * 60);
+    const [time, setTime] = useState(0.1 * 60);
     const [isActive, seIstActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false)
 
@@ -35,7 +35,7 @@ export function CountdownProvider({children}: CountdownProviderProps) {
     function resetCountdown() {
         clearTimeout(countdownTimeout)
         seIstActive(false)
-        setTime(1 * 60)
+        setTime(0.1 * 60)
         setHasFinished(false)
         
     }
