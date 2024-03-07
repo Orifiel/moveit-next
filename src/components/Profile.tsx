e1a76605cbdbd607fcf65b586f319f5504a44a84
Cleiton <49954230+Orifiel@users.noreply.github.com>
2024-03-07 13:53:04 -0300

    new change

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,7 +11,7 @@ export function Profile() {
         <div className={styles.profileContainer}>
             <img src="https://media.newyorker.com/photos/5ba177da9eb2f7420aadeb98/4:3/w_1229,h_921,c_limit/Cohen-Linus-Torvalds.jpg" alt=""/>
             <div>
-                <strong>Linus Torvalds</strong>
+                <strong>Puinguim Linux</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
                     Level {level}
